df6cf97db6f29068f9bd2a906f1a7d57d23adda4
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-09-08 11:38:43 -0500

    still setting up files for frontend of site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsCardComponent } from './components/news-card/news-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    newsApiUrl: 'https://newsapi.org/v2/top-headlines?country=jp&apiKey=YOUR_API_KEY',
+  };
